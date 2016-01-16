bf59894a14c87f603d6d56648970362d08b85043
muupu <once645@gmail.com>
2016-01-16 13:55:33 +0800

    init

---
+++ LongestCommonSubsequence.cc
@@ -0,0 +1,18 @@
+#include <vector>  
+#include <string>  
+#include <iostream> 
+
+namespace algorithm {
+
+	template <typename T>
+	void longest_common_subsequence(const T& s1, const T& s2, T& s3)  
+	{
+
+
+	}  
+
+	int main()
+	{
+		return 0;
+	}
+}
